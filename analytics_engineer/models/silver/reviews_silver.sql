28c83baf79b91186445b2ec4dedf7706fbce44b4
Taty Elvas <thaisaelvas@gmail.com>
2024-04-28 19:54:27 -0300

    Add silver models folder

---
+++ analytics_engineer/models/silver/reviews_silver.sql
@@ -0,0 +1,19 @@
+WITH
+    reviews_bronze_notnull AS (
+        SELECT *
+        FROM reviews_bronze
+        WHERE
+            listing_id IS NOT NULL
+            AND id IS NOT NULL
+            AND date IS NOT NULL
+            AND reviewer_id IS NOT NULL
+            AND reviewer_name IS NOT NULL
+            AND comments IS NOT NULL
+    ),
+    final AS (
+        SELECT DISTINCT
+            listing_id, id, date, reviewer_id, reviewer_name, comments
+        FROM reviews_bronze_notnull
+    )
+
+SELECT * FROM final
